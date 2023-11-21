2d65c9f28a10c800991cce9ba08607c0c9bad71b
CleytonDev1000 <150192920+CleytonDev1000@users.noreply.github.com>
2023-11-21 16:10:25 -0300

    Update LogAluno.jsx

--- src/pages/LogarAluno/LogAluno.jsx
+++ src/pages/LogarAluno/LogAluno.jsx
@@ -46,7 +46,7 @@ export default function LogAluno(){
                         <div>
                             <label for="email" class="block text-sm font-medium leading-6 text-red-400">CPF</label>
                             <div class="mt-2">
-                                <input onChange={(e)=>{setCpf(e.target.value)}} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-[10px]" placeholder="logue com os ultimos 4 digitos"></input>
+                                <input type="number" onChange={(e)=>{setCpf(e.target.value)}} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-[10px]" placeholder="logue com os ultimos 4 digitos"></input>
                             </div>
                         </div>
                       
