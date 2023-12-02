8df8911bc470bfe5cf3b404fbe54aa761e0a35c3
CleytonDev1000 <150192920+CleytonDev1000@users.noreply.github.com>
2023-12-02 18:22:52 -0300

    Update LogAluno.jsx

--- src/pages/LogarAluno/LogAluno.jsx
+++ src/pages/LogarAluno/LogAluno.jsx
@@ -6,23 +6,32 @@ import axios from "axios";
 export default function LogAluno(){
     const [cpf, setCpf] = useState("")
     const navigate = useNavigate();
+    const [response, setResponse] = useState("")
 
     async function Logando(e){
         e.preventDefault();
-        if(cpf.length < 4){
-            alert("Cpf invalido!")
-            return false
+        if (cpf == ""){
+            setResponse("O campo esta vazio!")
+            return
+        }
+        else if(cpf.length > 11){
+            setResponse("Cpf invalido!")
+            return
         }
         try {
-            let response = await axios.get("https://sampa.pythonanywhere.com/Alunos/?format=json")
+            const Formdata = new FormData()
+            Formdata.append("cpf", cpf)
+            let response = await axios.post("https://sampa.pythonanywhere.com/logar/", Formdata)
             response = response.data
-            for (let c = 0; c < response.length+1; c++){
-                if(response[c].cpf.slice(-4) == cpf.slice(-4)){
-                    navigate("/Termos")
-                    localStorage.setItem("nome", response[c].nome)
-                    localStorage.setItem("email_aluno", response[c].email)
-                    localStorage.setItem("cpf_aluno", response[c].cpf)
-                }
+            if(response.return){
+                navigate("/Termos")
+                localStorage.setItem("nome", response.nome)
+                localStorage.setItem("email_aluno", response.email)
+                localStorage.setItem("cpf_aluno", response.cpf)
+            }
+            else {
+                setResponse("O cpf não foi encontrado...")
+                return
             }
         }
         catch(error) {
@@ -46,12 +55,13 @@ export default function LogAluno(){
                         <div>
                             <label for="email" class="block text-sm font-medium leading-6 text-red-400">CPF</label>
                             <div class="mt-2">
-                                <input type="number" onChange={(e)=>{setCpf(e.target.value)}} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-[10px]" placeholder="logue com os ultimos 4 digitos"></input>
+                                <input type="number" onChange={(e)=>{setCpf(e.target.value)}} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-[10px]" placeholder="logue com o seu cpf"></input>
                             </div>
                         </div>
                       
                         <button type="submit" onClick={(e)=>{Logando(e)}} class="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Entrar</button>
                     </form>
+                    <h1 class="text-red-500">{response}</h1>
                     <p class="mt-10 text-center text-sm text-gray-500">
                         Não tem cadastro?
                         <a href="/Cadastramento" class="font-semibold leading-6 text-red-600 hover:text-red-700"> Cadastre-se</a>
