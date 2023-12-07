418b9fdbb8d1c6b558a50a34139ce2c33c3fa40d
CleytonDev1000 <150192920+CleytonDev1000@users.noreply.github.com>
2023-12-06 21:29:51 -0300

    Update camera.jsx

--- src/componentes/camera/camera.jsx
+++ src/componentes/camera/camera.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Camera() {
   const webcamRef = React.useRef(null);
-  const [countdown, setCountdown] = useState(1000);
+  const [countdown, setCountdown] = useState(10);
   const [preDetect, setpreDetect] = useState("");
   const navigate = useNavigate();
   const [cont, setCont] = useState(1)
@@ -13,12 +13,13 @@ export default function Camera() {
  
   useEffect(() => {
     const timer = setInterval(() => {
+      Check_email();
       if (countdown > 0) {
         setCountdown(countdown - 1);
         Pre_detect();
       } 
-      else if(countdown == 1){
-        if (cont != 3){
+      else if(countdown == 0){
+        if (cont < 3){
           setCont(cont + 1)
           setModal(true)
         }
@@ -27,10 +28,7 @@ export default function Camera() {
           return
         }
       }
-      else {
-        return
-      }
-    }, 1600);
+    }, 1100);
     return () => clearInterval(timer);
   }, [countdown]);
 
@@ -39,16 +37,30 @@ export default function Camera() {
     setModal(false)
   }
 
+  function Check_email(){
+    if(!localStorage.getItem("email_aluno")){
+      alert("Seu email nao foi detectado, vamos te enviar para a pagina de login!")
+      navigate("/")
+      return
+    }
+
+  }
+
   async function Pre_detect() {
     const imageSrc = webcamRef.current.getScreenshot();
     const Pre_formdata = new FormData();
     const blob = await fetch(imageSrc).then((r) => r.blob());
     Pre_formdata.append("imagem", blob, "imagem.png");
     Pre_formdata.append("email", localStorage.getItem("email_aluno"));
+    console.log("ENviei")
     const response = await axios.post("https://sampa.pythonanywhere.com/enviar_img/", Pre_formdata);
+    console.log(response.data)
 
     setpreDetect(response.data.response);
     if(response.data.return){
+      localStorage.removeItem("cpf_aluno")
+      localStorage.removeItem("email_aluno")
+      localStorage.removeItem("nome")
       navigate("/aprovado")
       return
     }
@@ -75,37 +87,45 @@ export default function Camera() {
           <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
           <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-  <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-      <div class="sm:flex sm:items-start">
-        <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-          <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-          </svg>
-        </div>
-        <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-          <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Falha na detecção</h3>
-          <div class="mt-2">
-            <p class="text-sm text-gray-500">Não foi possivel detectar EPI no seu corpo </p>
+          <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+              <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                </svg>
+              </div>
+              <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                {cont == 2 ? (
+                  <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Falha na detecção você ainda tem mais duas chances!</h3>  
+                ) : (
+                  false
+                )} 
+                {cont == 3 ? (
+                  <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Falha na detecção você ainda tem mais uma chance!</h3>  
+                ) : (
+                  false
+                )}
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">Não foi possivel detectar EPI no seu corpo </p>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-          <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button type="button" onClick={(e)=>{Cowndown(e)}} class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" >Refazer verificação</button>
-              <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"><a href="/">Cancelar</a></button>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button type="button" onClick={(e)=>{Cowndown(e)}} class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" >Refazer verificação</button>
+                    <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"><a href="/">Cancelar</a></button>
+                </div>
+              </div>
+            </div>
+              </div>
           </div>
-        </div>
-      </div>
-        </div>
-    </div>
         )}
-           
         </>
       ) : (
-        console.log("OI")
+        false
       )}  
       <div className="bg-[url('../public/fundo-transparente-png7.png')] bg-cover bg-center h-screen">
         <div
@@ -135,9 +155,7 @@ export default function Camera() {
               <p className="text-[20px] xl:text-[50px]">
                 3- Espere a verificação
               </p>
-              <p className="text-[20px] xl:text-[50px]">
-                4- Coloque a camera no nivel do seu rosto
-              </p>
+              {/*<button className="w-52 h-12 rounded-xl shadow-black bg-red-600 mt-14"><h1 className="text-white text-2xl">Testar</h1></button>*/}
               {countdown > 0 ? (
                 <p className="h-10 w-96 text-2xl ml-[5%] xl:text-[50px] ">
                   {countdown}
